2faa9588f8856eb25094abca9a75bd4735cce95e
YN <yn@Ahmed-Gamal.local>
2025-04-17 01:48:00 +0200

    Initial commit

---
+++ components/Roadmap/index.js
@@ -0,0 +1,291 @@
+import React from "react";
+import Styles from "./Roadmap.module.css";
+export default function index() {
+  return (
+    <div className={Styles["dev-roadmap-cont"]}>
+      <div className={Styles["dev-roadmap-left"]}>
+        <div className={Styles["dev-roadmap-mint"]}>
+          
+          <h4 className={Styles["dev-roadmap-mint-title"]}>1- First  Step HTML ♡</h4>
+          <ul className={Styles["dev-roadmap-list"]}>
+            <li> HTML Basics 🤔</li>
+            <li>Semantic HTML🤝</li>
+            <li>Forms and Validations ⚡️ </li>
+             <li> Accessibility ⚡️ </li>
+        
+            
+
+          </ul>
+        </div>
+        <div className={Styles["dev-roadmap-mint "]}>
+          <h4 className={Styles["dev-roadmap-mint-title"]}> 2- SECOND Step CSS 🥳</h4>
+          <ul className={Styles["dev-roadmap-list"]}>
+            <li>Learn CSS Basics            </li>
+            <br/>
+
+            <li>  Flexbox for one-dimensional layouts (rows or columns)</li>
+                        <br/>
+
+            <li>
+             CSS Grid for two-dimensional layouts
+              </li> 
+              <br/>
+
+              <li>
+              Responsive design principles for adaptability across devices
+              </li>
+              <br/>
+
+              <li>
+              CSS frameworks like Bootstrap or Tailwind for rapid development
+              </li>
+              <br/>
+
+              <li>
+              Custom CSS properties (variables) for consistent styling
+              </li>
+              <br/>
+
+              <li>
+              Media queries for device-specific adjustments
+              </li>
+              <br/>
+
+              <li>
+              CSS positioning and float for specific element placement
+              </li>
+              <br/>
+
+              <li>
+              CSS Preprocessors
+
+              </li>
+              <br/>
+
+              <li>(Post Css & Sass)</li>
+              <br/>
+
+              <li>CSS Architecture              </li>
+              <br/>
+              <li>CBox Model             </li>
+              <br/><li>CSS - Styling Lists             </li>
+              <br/><li>CCSS - visibility            </li>
+              <br/><li>CSS - Grid Layout              </li>
+              <br/><li>CSS - Flexbox             </li>
+              <br/><li>CSS - Animations             </li>
+              <br/><li>CSS - Units </li>
+              <br/><li>CSS - Responsive Design               </li>
+              <br/><li>CSS - Seo             </li>
+              <br/><li>Seo on Web             </li>
+              <br/><li>CSS - Pseudo Classes             </li>
+              <br/><li>CSS - Pseudo Elements</li>
+              <br/><li>CSS - Text Effects              </li>
+              <br/><li>CCSS - Validations            </li>
+              <br/><li>CSS - Math Functions             </li>
+              <br/><li>CSS - mask Property             </li>
+              <br/><li>CSS RWD Frameworks            </li>
+              <br/>
+              <li>CSS - Specificity            </li>
+              <br/><li>CSS - Pagination</li>
+              <br/><li>CSS - Media Queries              </li>
+              <br/>
+            <li>Making layouts            </li>
+            <br/>
+
+            <li>CSS - Box Sizing          </li>
+            <br/>
+
+          </ul>
+        </div>
+      </div>
+      <div className={Styles["dev-roadmap-right"]}>
+        <div className={Styles["dev-roadmap-mint"]}>
+          <h4 className={Styles["dev-roadmap-mint-title"]}> 3- FrameWorks  Css♡</h4>
+          <ul className={Styles["dev-roadmap-list"]}>
+            <li>Tailwind CSS </li>
+            <br/>
+
+            <li>BootStrap 5</li>
+          </ul>
+        </div>
+        <div className={Styles["dev-roadmap-mint"]}>
+          <h4 className={Styles["dev-roadmap-mint-title"]}>4-  JavaScript🥰</h4>
+          <ul className={Styles["dev-roadmap-list"]}>
+            <li> What is JavaScript?
+            </li>
+            <br/>
+            <li> History of JavaScript
+            </li>
+            <br/>
+
+            <li>Javascript Versions
+            </li>
+            <br/>
+
+            <li> How to Run Javascript
+            </li>
+            <br/>
+
+            <li> Javascript Variables
+            </li>
+            <br/>
+
+            <li> Hoisting </li>
+            <br/>
+
+            <li>  Datatypes </li>
+            <br/>
+
+            <li> Object </li>
+            <br/>
+
+            <li> Built-in objects
+            </li>
+            <br/>
+
+            <li> Data Structures
+            </li>
+            <br/>
+
+            <li> Equality Comparisons
+            </li>
+            <br/>
+
+            <li> Loops and Iterations
+            </li>
+            <br/>
+
+            <li> Control Flow
+            </li>
+            <br/>
+
+            <li> Expressions and Operators
+            </li>
+
+            <br/>
+
+<li> Functions
+
+</li>
+
+<br/>
+
+<li> DOM APIs
+
+</li>
+
+<br/>
+
+<li> Strict Mode
+
+</li>
+
+<br/>
+
+<li> This Keyword
+
+</li>
+
+<br/>
+
+<li> Asynchronous JavaScript
+
+</li>
+
+<br/>
+
+<li> Working with APIs
+
+</li>
+
+<br/>
+
+<li> Classes
+</li>
+
+<br/>
+
+<li> Javascript Iterators and Generators
+
+</li>
+
+<br/>
+
+<li> Modules
+</li>
+<br/>
+
+<li> Memory Management
+
+</li>
+<br/>
+
+<li> JavaScript Chrome Dev Tools
+
+</li>
+<br/>
+
+<li> ES6+
+</li>
+<br/>
+
+<li color="red"> React Content❤️‍🔥↓
+</li>
+<br/>
+
+<li> CLI Tools
+
+</li>
+<br/>
+
+<li> Components
+</li>
+<br/>
+
+<li> Rendering
+</li>
+<br/>
+
+<li> Hooks
+</li>
+<br/>
+
+<li> Routing
+</li>
+<br/>
+
+<li> Component / Libraries
+
+</li>
+<br/>
+
+<li> Headless Component Libraries
+
+</li><br/>
+
+<li> API Calls
+
+</li><br/>
+
+<li>  Frameworks
+
+</li><br/>
+
+<li> Forms
+</li><br/>
+
+<li> Types & Validation
+
+</li><br/>
+
+<li> Animation
+</li><br/>
+
+<li> Error Boundaries
+</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
